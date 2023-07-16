9d0acd9965d2789c1f56198e068ab12476bdb0bd
itbolt <minhtien.nguyen345@gmail.com>
2023-07-15 23:29:20 -0700

    final

--- server.py
+++ server.py
@@ -10,6 +10,10 @@ def echo():
     response = f"{text} {text}"
     return response
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
