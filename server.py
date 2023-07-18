4af5fc6eb8f673905ad7e9ea72f6da955e3a6fdf
itbolt <minhtien.nguyen345@gmail.com>
2023-07-17 22:30:48 -0700

    day4?

--- server.py
+++ server.py
@@ -2,46 +2,47 @@ import os
 from flask import Flask, request, jsonify, render_template
 import openai
 
-openai.api_key = os.environ.get("OPENAI_KEY")
-print(models.data[0].id)
+#print(models.data[0].id)
 
 # create a chat completion
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
+#chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
 
 # print the chat completion
-print(chat_completion.choices[0].message.content)
+#print(chat_completion.choices[0].message.content)
 
+openai.api_key = os.environ.get("OPENAI_KEY")
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
 
 @app.route('/echo', methods=['POST'])
 def echo():
     message = request.form.get('message')
     return message + " " + message
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
 @app.route('/chat', methods=['POST'])
-def chat():
+def chat_with_openai():
     data = request.get_json()
-    user_message = data['message']
+    user_input = data.get('text', '')
     
-    # Call the OpenAI API to get a response
+    # Call the OpenAI API to get the chat response
     response = openai.Completion.create(
-        engine="text-davinci-002",  # You can choose a different engine if needed
-        prompt=user_message,
-        max_tokens=150,
-        temperature=0.7,
-        n=1,
-        stop=None
+        engine="davinci",
+        prompt=user_input,
+        max_tokens=100,
     )
     
-    # Extract the response text from the API response
-    chat_response = response['choices'][0]['text'].strip()
-    
-    return chat_response
+    chat_response = response.choices[0].text.strip()
+    return jsonify({'response': chat_response})
+
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
