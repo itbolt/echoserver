3b9c8096829d70bb2f8f773ba2d78701e38b6e29
itbolt <minhtien.nguyen345@gmail.com>
2023-07-23 21:48:34 -0700

    like 39

--- server.py
+++ server.py
@@ -2,19 +2,20 @@ import os
 from flask import Flask, request, jsonify, render_template
 import openai
 import pymongo
+from pymongo import MongoClient
 
 uri = "mongodb+srv://mnguyen:Ntmntm1019@cluster0.ybulhme.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 cluster = MongoClient(uri)
 # Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')
+    cluster.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
 app = Flask(__name__)
 
-db = mongo_client['NuocDB']
+db = cluster['NuocDB']
 collection = db['ResponseLog']
 
 app = Flask(__name__)
